4317ee59c5b5fa5106c265dadd87b32f5b6a1fbd
Alfonso Niccolo Sales <84696659+AlfonsoSales@users.noreply.github.com>
2024-05-21 14:48:05 +0800

    Working Player Controller script
    Player Controller script has functions and such in it that are called on keypress, just insert function call to activate a skill from the character class, etc. add extra conditionals as well to optimize.

--- TheFALLEN/Assets/Scripts/Player_Controller.cs
+++ TheFALLEN/Assets/Scripts/Player_Controller.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Player_Controller : MonoBehaviour
 {
     private float horizontal;
-    private float speed = 0f;
+    private float speed = 8f;
     private float jumpPower = 16f;
     private bool isFacingRight = true;
 
@@ -29,11 +29,22 @@ public class Player_Controller : MonoBehaviour
         }
 
         Flip();
+        BaseAtk();
+        Skill1();
+        Skill2();
+        Skill3();
+        Skill4();
+        CharaSwap1();
+        CharaSwap2();
+        CharaSwap3();
+        CharaSwap4();
+        CharaSwap5();
     }
 
     private void FixedUpdate()
     {
         rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+      
     }
 
     private bool IsGrounded()
@@ -49,6 +60,89 @@ public class Player_Controller : MonoBehaviour
             Vector3 localScale = transform.localScale;
             localScale.x *= -1f;
             transform.localScale = localScale;
+        }
+    }
+
+    private void BaseAtk()
+    {
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            //call basic attack function here
+            Debug.Log("Attack!");
+        }
+    }
+
+    private void Skill1()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            //call skill 1 function here
+            Debug.Log("skill 1 active");
+        }
+    }
+    private void Skill2()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            //call skill 2 function here
+            Debug.Log("skill 2 active");
+        }
+    }
+    private void Skill3()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            //call skill 3 function here
+            Debug.Log("skill 3 active");
+        }
+    }
+    private void Skill4()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            //call skill 4 function here
+            Debug.Log("skill 4 active");
+        }
+    }
+
+    private void CharaSwap1()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            //call Hero Swap function
+            Debug.Log("Hero Active");
+        }
+    }
+    private void CharaSwap2()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            //call Lancer Swap Function
+            Debug.Log("Lancer Active");
+        }
+    }
+    private void CharaSwap3()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            //call Archer Swap Function
+            Debug.Log("Archer Active");
+        }
+    }
+    private void CharaSwap4()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            //call Wizard Swap Function
+            Debug.Log("Wizard Active");
+        }
+    }
+    private void CharaSwap5()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha5))
+        {
+            //call Saint Swap Function
+            Debug.Log("Saint Active");
         }
     }
 }
