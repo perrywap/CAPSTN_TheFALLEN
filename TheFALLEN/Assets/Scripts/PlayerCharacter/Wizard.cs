8385f6420748e751266c69963e2e6226487bc6b9
Alfonso Niccolo Sales <84696659+AlfonsoSales@users.noreply.github.com>
2024-07-08 15:20:29 +0800

    Update Wizard float skill
    Float skill logic is here just need to link it to player rigidbody when activated

--- TheFALLEN/Assets/Scripts/PlayerCharacter/Wizard.cs
+++ TheFALLEN/Assets/Scripts/PlayerCharacter/Wizard.cs
@@ -4,10 +4,28 @@ using UnityEngine;
 
 public class Wizard : Player
 {
+    [SerializeField] private float LightSkillCoolDownTime;
+    [SerializeField] private float LightSkillNextTime;
+    [SerializeField] private float LightSkillActiveTime;
+    [SerializeField] private float LightSkillOffTime;
     #region OVERRIDABLE FUNCTIONS
     public override void ActivateSupportSkill()
     {
         Debug.Log("Wizard is using FLOAT skill");
+        if (Time.time - LightSkillNextTime < LightSkillCoolDownTime)
+        {
+            return;
+        }
+        LightSkillNextTime = Time.time;
+
+        Rigidbody2d rb2d = GetComponent<Rigidbody2D>();
+        rb2d.enabled = false;
+        if (Time.time - LightSkillActiveTime < LightSkillOffTime)
+        {
+            rb2d.enabled = true;
+        }
+
+        
     }
 
     public override void ActivateLightSkill()
